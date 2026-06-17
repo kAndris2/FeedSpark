dfb818742d0da4aecd49186b55c9f63fa78789b0
kAndris2 <k.andris258@gmail.com>
2026-06-17 17:07:20 +0200

    logger settings sdded to base config

--- src/Interfaces/IAppSettings.ts
+++ src/Interfaces/IAppSettings.ts
@@ -1,7 +1,9 @@
 import { IAiStudioSettings } from "./IAiStudioSettings";
+import { ILoggerSettings } from "./ILoggerSettings";
 import { IYoutubeSettings } from "./IYoutubeSettings";
 
 export interface IAppSettings {
+    loggerSettings: ILoggerSettings;
     aiStudioSettings: IAiStudioSettings;
     youtubeSettings: IYoutubeSettings;
 }
--- src/Models/AppSettings.ts
+++ src/Models/AppSettings.ts
@@ -4,20 +4,25 @@ import { ISelfConstructible } from "../Interfaces/ISelfConstructible";
 import { IYoutubeSettings } from "../Interfaces/IYoutubeSettings";
 import { AiStudioSettings } from "./AiStudioSettings";
 import { YoutubeSettings } from "./YoutubeSettings";
+import { ILoggerSettings } from "../Interfaces/ILoggerSettings";
+import { LoggerSettings } from "./LoggerSettings";
 
 export class AppSettings implements IAppSettings, ISelfConstructible<IAppSettings> {
+    loggerSettings!: ILoggerSettings;
     aiStudioSettings!: IAiStudioSettings;
     youtubeSettings!: IYoutubeSettings;
 
     constructor(settings?: IAppSettings) {
         if (!settings) return;
 
+        this.loggerSettings = new LoggerSettings(settings.loggerSettings);
         this.aiStudioSettings = new AiStudioSettings(settings.aiStudioSettings);
         this.youtubeSettings = new YoutubeSettings(settings.youtubeSettings);
     }
 
     createDefault(): IAppSettings {
         return {
+            loggerSettings: new LoggerSettings().createDefault(),
             aiStudioSettings: new AiStudioSettings().createDefault(),
             youtubeSettings: new YoutubeSettings().createDefault()
         };
